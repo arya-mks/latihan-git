520983571b3d4f3248737b005f9aec3e7298a71f
arya-mks <akrendsg@gmail.com>
2024-11-23 11:58:29 +0800

    tambah Laravel

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <table width="100%" body="5px">
     <tr><td>NAMA SAYA DAUD ARYA </td></tr>
     <tr><td>belajar FULL STACK</td></tr>
+    <tr><td>Menggunakan LARAVEL</td></tr>
 </table>
 </BODY>
 </HTML>
